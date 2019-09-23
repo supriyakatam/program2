2f9845ff67d765bbe776deaa63cb5d61fb20d480
supriya <50396195+supriyakatam@users.noreply.github.com>
2019-09-23 10:21:51 +0530

    test.c

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int c, k;
+ 
+  printf("Enter number of rows\n");
+  
+ 
+  for (c = 1; c <= 5; c++)
+  {
+    for(k = 1; k <= c; k++)
+      printf("*");
+ 
+    printf("\n");
+  }
+ 
+  return 0;
+}
